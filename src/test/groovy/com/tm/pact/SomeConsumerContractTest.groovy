4dc16320699dad7287c7020ea69367855bc2cf76
TM <tomaszmuc@gmail.com>
2020-11-22 12:56:09 +0100

    add basic consumer-driven test

--- src/test/groovy/com/tm/pact/SomeConsumerContractTest.groovy
+++ src/test/groovy/com/tm/pact/SomeConsumerContractTest.groovy
@@ -1,4 +1,45 @@
 package com.tm.pact
 
+import au.com.dius.pact.consumer.PactVerificationResult
+import au.com.dius.pact.consumer.groovy.PactBuilder
+import groovyx.net.http.RESTClient
+import org.junit.jupiter.api.Test
+
 class SomeConsumerContractTest {
+    @Test
+    void "Some pact between SomeConsumer and SomeProvider"() {
+
+        def pact = new PactBuilder()
+                .with {
+                    serviceConsumer "SomeConsumer"
+                    hasPactWith "SomeProvider"
+                    port 8080
+
+                    uponReceiving('a retrieve hello request')
+                    withAttributes(method: 'post', path: '/hello')
+                    withBody {
+                        name string()
+                    }
+                    willRespondWith(
+                            status: 200,
+                            headers: ['Content-Type': 'application/json'],
+                    )
+                    withBody {
+                        greeting string()
+                    }
+                }
+
+        PactVerificationResult result = pact.runTestAndVerify {
+            def client = new RESTClient('http://localhost:8080/')
+            def response = client.post(
+                    path: '/hello',
+                    body: [name: 'Tom'],
+                    requestContentType: 'application/json'
+            )
+
+            assert response.status == 200
+            assert response.contentType == 'application/json'
+            assert response.data == ['greeting': 'xxx']
+        }
+    }
 }
